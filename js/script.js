309248a59fc2bd772d88d6e23d8966689d2bbc63
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-05 13:27:40 +0100

    feat: add JS structure

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// TASK:Calcolare il prezzo del bigliettto
+
+// 1 - Chiedere all'utente il numero di chilometri che intende percorrere
+// 2 - Chiedere all'utente l'età del passeggiero
+// 3 - Calcolare il prezzo del biglietto
+    // 3a- Calcolo del prezzo in base ai chilometri inseriti dall'utente
+    // 3b- Calcolo di un eventuale sconto del 20% ai minorenni
+    // 3c- Calcolo di un eventuale sconto del 40% agli over 65
+    // 3d- Calcolo del prezzo finale in base alle variabili indicate
+// 4 - Stampare a schermo il risultato per l'utente
