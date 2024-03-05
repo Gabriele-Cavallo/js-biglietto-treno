097c972602d126470ef692fdc91280ec9a35cd43
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-05 14:18:35 +0100

    feat: add to.Fixed(2) at finalPrice

--- js/script.js
+++ js/script.js
@@ -2,31 +2,27 @@
 
 // 1 - Chiedere all'utente il numero di chilometri che intende percorrere
 const userTripKm = parseInt(prompt('Ciao, dimmi quanti chilometri devi percorrere!!'))
-console.log(userTripKm);
 // 2 - Chiedere all'utente l'età del passeggiero
 const userAge = parseInt(prompt('Ciao, dimmi quanti anni hai!!'))
-console.log(userAge);
 // 3 - Calcolare il prezzo del biglietto
     // 3a- Calcolo del prezzo in base ai chilometri inseriti dall'utente (considerando un valore di 0.21€ al km)
     const userPriceKM = userTripKm * 0.21
-    console.log(userPriceKM);
     // 3b- Calcolo di un eventuale sconto del 20% ai minorenni
     if (userAge < 18){
         userDiscount20 = userPriceKM - (userPriceKM * 20 / 100);
     }
-    // console.log(userDiscount20);
     // 3c- Calcolo di un eventuale sconto del 40% agli over 65
     if (userAge > 65){
         userDiscount40 = userPriceKM - (userPriceKM * 40 / 100);
     }
-    // console.log(userDiscount40);
     // 3d- Calcolo del prezzo finale in base alle variabili indicate
-    let finalPrice = 'Il prezzo del tuo bilgietto è di €' + userPriceKM;
+    let userMessage = 'Il prezzo del tuo bilgietto è di €'
+    let finalPrice = userPriceKM;
     if (userAge < 18){
-        finalPrice = 'Il prezzo del tuo bilgietto è di €' + userDiscount20;
+        finalPrice = userDiscount20;
     }else if(userAge > 65){
-        finalPrice = 'Il prezzo del tuo bilgietto è di €' + userDiscount40;
+        finalPrice = userDiscount40;
     }
-    console.log(finalPrice);
+    finalPrice = finalPrice.toFixed(2);
 // 4 - Stampare a schermo il risultato per l'utente
-document.getElementById('price').innerHTML = finalPrice;
+document.getElementById('price').innerHTML = `${userMessage} ${finalPrice}`;
